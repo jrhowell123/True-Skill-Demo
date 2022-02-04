d634d11c926ce3470fb9d1ced78a9def241fa40b
jrhowell123 <jrhowell@ucsd.edu>
2022-02-04 15:56:33 -0800

    fixed errors

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,11 +4,11 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void subtraction(){
-        assertEquals(2, SkillDemo.subtract(3,3));//should fail
+        assertEquals(2, SkillDemo.subtract(3,1));//should pass
     }
 
     @Test
     public void multiplication(){
-        assertEquals(6, SkillDemo.multiply(3,3));//should fail
+        assertEquals(6, SkillDemo.multiply(3,2));//should pass
     }
 }
